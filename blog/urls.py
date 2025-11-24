e98ee227bf7a88aaeb6ee363d356db35549d161d
David Molina <davidjmolina55@gmail.com>
2025-11-24 16:48:54 +0000

    add comment crud

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,11 @@ from . import views
 from django.urls import include, path
 
 urlpatterns = [
-    path("about/", include("about.urls"), name="about-urls"),
+    path("about/", include("about.urls")),
     path("", views.PostList.as_view(), name="home"),
+    path('<slug:slug>/edit_comment/<int:comment_id>/',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>/',
+         views.comment_delete, name='comment_delete'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
